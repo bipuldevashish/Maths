cce20fb3c986b488e3288811cb85924b5a1a3a91
Bipul Devashish <bipuldevashish@gmail.com>
2021-04-05 13:06:55 +0530

    added intro activity..

--- app/src/main/java/com/bipuldevashish/maths/ui/Intro.java
+++ app/src/main/java/com/bipuldevashish/maths/ui/Intro.java
@@ -2,15 +2,49 @@ package com.bipuldevashish.maths.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ViewFlipper;
 
 import com.bipuldevashish.maths.R;
 
 public class Intro extends AppCompatActivity {
 
+    ViewFlipper viewFlipper;
+    Button btn;
+    int[] image = {
+            R.drawable.insider_ic_online_test,
+            R.drawable.insider_study_material,
+            R.drawable.insider_ic_discussion,
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro);
+        getSupportActionBar().hide();
+        btn = findViewById(R.id.btn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intro.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+        viewFlipper = findViewById(R.id.viewflipper);
+        for (int i = 0; i < image.length; i++) {
+            flip_image(image[i]);
+        }
+    }
+    private void flip_image(int i) {
+        ImageView view = new ImageView(this);
+        view.setBackgroundResource(i);
+        viewFlipper.addView(view);
+        viewFlipper.setFlipInterval(4000);
+        viewFlipper.setAutoStart(true);
+        viewFlipper.setInAnimation(this, android.R.anim.slide_in_left);
+        viewFlipper.setOutAnimation(this, android.R.anim.slide_out_right);
     }
 }
