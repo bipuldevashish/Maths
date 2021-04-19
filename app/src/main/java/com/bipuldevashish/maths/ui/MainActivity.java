0031cc6daa98bfffa178f5ad8135e176298990b1
Bipul Devashish <bipuldevashish@gmail.com>
2021-04-19 12:18:30 +0530

    updated code..

--- app/src/main/java/com/bipuldevashish/maths/ui/MainActivity.java
+++ app/src/main/java/com/bipuldevashish/maths/ui/MainActivity.java
@@ -4,29 +4,22 @@ import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-import androidx.coordinatorlayout.widget.CoordinatorLayout;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
-import com.bipuldevashish.maths.MyApplication;
 import com.bipuldevashish.maths.R;
-import com.bipuldevashish.maths.util.ConnectivityReceiver;
 import com.google.android.material.navigation.NavigationView;
-import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity
         implements ConnectivityReceiver.ConnectivityReceiverListener {
